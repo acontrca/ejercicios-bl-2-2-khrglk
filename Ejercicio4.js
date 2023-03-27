54d5d2a347a9808dc4c5dc66f1820540201cd4de
acontrca <126878811+acontrca@users.noreply.github.com>
2023-03-27 18:28:26 -0300

    Initial commit

---
+++ Ejercicio4.js
@@ -0,0 +1,10 @@
+'Use strict';
+
+let personas = [
+  { nombre: 'Juanma', apellido: 'Cano', edad: 22 },
+  { nombre: 'Juanma', apellido: 'Armario', edad: 45 },
+  { nombre: 'David', apellido: 'Quiñones', edad: 18 },
+  { nombre: 'Alberto', apellido: 'Rus', edad: 18 },
+  { nombre: 'Alberto', apellido: 'Rus', edad: 24 },
+  { nombre: 'Alberto', apellido: 'Rodriguez', edad: 80 },
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+# ejercicios-bl-2-2-khrglk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ejercicios-bl-2-2-khrglk)
